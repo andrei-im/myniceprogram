b95278186b437ea22d375fff515477d7f945f41e
Andrei <andreiimotroc@gmail.com>
2023-10-05 13:21:38 +0300

    First commit

---
+++ cmd/web/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/andrei-im/myniceprogram/pkg/config"
+	"github.com/andrei-im/myniceprogram/pkg/handlers"
+	"github.com/andrei-im/myniceprogram/pkg/render"
+)
+
+const portNumber = ":8080"
+
+// main is the main application function
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
